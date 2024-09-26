70163a6d2e42f4e9a217415d68250156a6f23f8c
Hubert Zięba <hubzieba@gmail.com>
2024-09-27 00:39:22 +0200

    Change syntax for link matchers

--- lua/git-remote/config.lua
+++ lua/git-remote/config.lua
@@ -1,6 +1,6 @@
 ---@alias url_regex string Compared with remote repository link.
 ---@alias link_template string Should contain "${base_url}", "${ref}" and "${filepath}" placeholders.
----@alias link_matchers [url_regex, link_template][]
+---@alias link_matchers table<url_regex, link_template>
 
 ---@alias command string Command for opening links.
 ---@alias browsers command[]
@@ -17,8 +17,8 @@
 ---@type opts
 local defaults = {
 	link_matchers = {
-		{ "^https?://[^/]*gitlab[^/]*/.*$", "${base_url}/-/blob/${ref}/${filepath}" },
-		{ "^https?://[^/]*github[^/]*/.*$", "${base_url}/blob/${ref}/${filepath}" },
+		["^https?://[^/]*gitlab[^/]*/.*$"] = "${base_url}/-/blob/${ref}/${filepath}",
+		["^https?://[^/]*github[^/]*/.*$"] = "${base_url}/blob/${ref}/${filepath}",
 	},
 	browsers = {
 		"xdg-open",
